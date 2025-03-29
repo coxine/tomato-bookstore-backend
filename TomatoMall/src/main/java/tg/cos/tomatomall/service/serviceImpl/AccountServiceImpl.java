ece6c79fc05b7bde8ce447fb6a6386292c0c3db5
231250116 <231250116@smail.nju.edu.cn>
2025-03-29 19:59:29 +0800

    fix: 根据文档,将updateUser的标识设置成了username而非token

--- TomatoMall/src/main/java/tg/cos/tomatomall/service/serviceImpl/AccountServiceImpl.java
+++ TomatoMall/src/main/java/tg/cos/tomatomall/service/serviceImpl/AccountServiceImpl.java
@@ -63,23 +63,28 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public String updateUser(AccountDTO accountDTO) {
 //        System.out.println("approach impl");
-        Account account=securityUtil.getCurrentUser();
-        if(account!=null) {    // 只更新非空字段
-            if (accountDTO.getUsername() != null) account.setUsername(accountDTO.getUsername());
-            if (accountDTO.getPassword() != null)
-            {
-                String encodedPassword = passwordEncoder.encode(accountDTO.getPassword());
-                account.setPassword(encodedPassword);
-            }
-            if (accountDTO.getName() != null) account.setName(accountDTO.getName());
-            if (accountDTO.getAvatar() != null) account.setAvatar(accountDTO.getAvatar());
-            if (accountDTO.getTelephone() != null) account.setTelephone(accountDTO.getTelephone());
-            if (accountDTO.getEmail() != null) account.setEmail(accountDTO.getEmail());
-            if (accountDTO.getLocation() != null) account.setLocation(accountDTO.getLocation());
-            accountRepository.save(account); // 更新已有用户
-            return "用户信息更新成功";
+//        Account account=securityUtil.getCurrentUser();
+        Account account;
+        Optional<Account> accountOptional = accountRepository.findByUsername(accountDTO.getUsername());
+        if (accountOptional.isPresent()) {
+            account = accountOptional.get();
+        }else {
+            return "用户不存在";
         }
-        return "用户不存在";
+        // 只更新非空字段
+//            if (accountDTO.getUsername() != null) account.setUsername(accountDTO.getUsername());
+        if (accountDTO.getPassword() != null)
+        {
+            String encodedPassword = passwordEncoder.encode(accountDTO.getPassword());
+            account.setPassword(encodedPassword);
+        }
+        if (accountDTO.getName() != null) account.setName(accountDTO.getName());
+        if (accountDTO.getAvatar() != null) account.setAvatar(accountDTO.getAvatar());
+        if (accountDTO.getTelephone() != null) account.setTelephone(accountDTO.getTelephone());
+        if (accountDTO.getEmail() != null) account.setEmail(accountDTO.getEmail());
+        if (accountDTO.getLocation() != null) account.setLocation(accountDTO.getLocation());
+        accountRepository.save(account); // 更新已有用户
+        return "用户信息更新成功";
     }
 
 }
